3d3d293479b6146ac4ec9fe73e164914eb86ceb8
Dastankt <kasymovdastan12@gmail.com>
2026-01-31 16:34:45 +0600

    Djang2

---
+++ lms/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hello/', views.hello, name='hello'),
+    path('time/', views.time, name='time'),
+    path('joke/', views.joke, name='joke'),
+    path('password/', views.password, name='password'),
+    path('count/', views.count, name='count'),
+    path('hello_time/', views.hello_time, name='hello_time'),
+    path('codecoin/', views.codecoin, name='codecoin'),
+    path('languages/', views.languages, name='languages'),
+    path('next_lesson/', views.next_lesson, name='next_lesson')
+]
