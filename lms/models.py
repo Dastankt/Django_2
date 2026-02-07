bcef8ee84a0adaf6216c3dfc9bfaf9fcfa63daa3
Dastankt <kasymovdastan12@gmail.com>
2026-02-07 16:26:38 +0600

    Django2 models

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.TextField(max_length=20)
+    last_name = models.TextField(max_length=20)
+    phone_number = models.IntegerField()
+    avatar = models.ImageField(upload_to='media/')
+
+
+class Mentor(models.Model):
+    first_name = models.TextField(max_length=20)
+    last_name = models.TextField(max_length=20)
+    phone_number = models.IntegerField()
+    avatar = models.ImageField(upload_to='media/')
+    is_mentor = models.BooleanField(default=True)
+
+
+class Task(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    codecoin = models.IntegerField()
+    is_completed = models.BooleanField(default=False)
+
+
+class Visit(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    is_visited = models.BooleanField(default=False)
+    date = models.DateField()
--- lms/views.py
+++ lms/views.py
@@ -1,56 +1,58 @@
 from django.http import HttpResponse
 from datetime import datetime
 import random, string
+from django.shortcuts import render
+from .models import *
 
-def hello(request):
-    return HttpResponse("Привет это работа views+urls")
-
-def time(request):
-    time_now = datetime.now()
-    return HttpResponse(time_now)
-
-def hello(request):
-    name = request.GET.get('name', 'Гость')
-    return HttpResponse(f"Привет, {name}!")
-
-# def add(request):
-#     a = int(request.GET.get('a', 0))
-#     b = int(request.GET.get('b', 0))
-#     return HttpResponse(f"Сумма: {a + b}")
-
-def joke(request):
-    jokes = ["Байт — это укус программиста.", "Python — не только змея!", "Тостер — хлебный сервер."]
-    return HttpResponse(random.choice(jokes))
-
-def password(request):
-    pwd = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-    return HttpResponse(f"Твой новый пароль: {pwd}")
-
-def count(request):
-    word = request.GET.get('word', '')
-    return HttpResponse(f"В слове '{word}' — {len(word)} букв.")
-
-
-def hello_time(request):
-    hour = datetime.now().hour
-    if hour < 12:
-        msg = "Доброе утро!"
-    elif hour < 18:
-        msg = "Добрый день!"
-    else:
-        msg = "Добрый вечер!"
-    return HttpResponse(msg)
-
-
-def codecoin(request):
-    coins = random.randint(1, 100)
-    return HttpResponse(f"Ты заработал {coins} CodeCoin!")
-
-def languages(request):
-    langs = ["Python", "JavaScript", "C++", "Dart", "Kotlin"]
-    return HttpResponse('<br>'.join(langs))
-
-def next_lesson(request):
-    today = datetime.today()
-    next_day = today + datetime.time(days=7)
-    return HttpResponse(f"Следующее занятие: {next_day.strftime('%d.%m.%Y')}")
+# def hello(request):
+#     return HttpResponse("Привет это работа views+urls")
+#
+# def time(request):
+#     time_now = datetime.now()
+#     return HttpResponse(time_now)
+#
+# def hello(request):
+#     name = request.GET.get('name', 'Гость')
+#     return HttpResponse(f"Привет, {name}!")
+#
+# # def add(request):
+# #     a = int(request.GET.get('a', 0))
+# #     b = int(request.GET.get('b', 0))
+# #     return HttpResponse(f"Сумма: {a + b}")
+#
+# def joke(request):
+#     jokes = ["Байт — это укус программиста.", "Python — не только змея!", "Тостер — хлебный сервер."]
+#     return HttpResponse(random.choice(jokes))
+#
+# def password(request):
+#     pwd = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+#     return HttpResponse(f"Твой новый пароль: {pwd}")
+#
+# def count(request):
+#     word = request.GET.get('word', '')
+#     return HttpResponse(f"В слове '{word}' — {len(word)} букв.")
+#
+#
+# def hello_time(request):
+#     hour = datetime.now().hour
+#     if hour < 12:
+#         msg = "Доброе утро!"
+#     elif hour < 18:
+#         msg = "Добрый день!"
+#     else:
+#         msg = "Добрый вечер!"
+#     return HttpResponse(msg)
+#
+#
+# def codecoin(request):
+#     coins = random.randint(1, 100)
+#     return HttpResponse(f"Ты заработал {coins} CodeCoin!")
+#
+# def languages(request):
+#     langs = ["Python", "JavaScript", "C++", "Dart", "Kotlin"]
+#     return HttpResponse('<br>'.join(langs))
+#
+# def next_lesson(request):
+#     today = datetime.today()
+#     next_day = today + datetime.time(days=7)
+#     return HttpResponse(f"Следующее занятие: {next_day.strftime('%d.%m.%Y')}")
